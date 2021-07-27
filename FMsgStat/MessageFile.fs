461c671ac9f6b0fec5d816076793e16ebe8d31f7
Piotr Romanowski <wsrh456@gmail.com>
2021-07-27 07:37:11 +0200

    Construct snakeCaseNamingPolicy in module scope

--- FMsgStat/MessageFile.fs
+++ FMsgStat/MessageFile.fs
@@ -11,28 +11,27 @@ type Message =
     { SenderName: string
       TimestampMs: int64
       Content: string }
-    
-type Contents = {
-    Participants: Participant []
-    Messages: Message []
-}
+
+type Contents =
+    { Participants: Participant []
+      Messages: Message [] }
 
 type T =
     { Participants: Participant []
       Messages: Message [] }
 
-type SnakeCaseNamingPolicy() =
-    inherit JsonNamingPolicy()
+let private snakeCaseNamingPolicyOptions =
+    JsonSerializerOptions(
+        PropertyNamingPolicy =
+            (let ti =
+                CultureInfo.GetCultureInfo("en-US").TextInfo
 
-    override x.ConvertName(name) =
-        let ti = CultureInfo("en-US", false).TextInfo
+             let re = Regex("(?<=\w)([A-Z])")
 
-        Regex("(?<=\w)([A-Z])").Replace(name, "_$1")
-        |> ti.ToLower
+             { new JsonNamingPolicy() with
+                 member _.ConvertName(x) = re.Replace(x, "_$1") |> ti.ToLower })
+    )
 
 let readFromFile fileName =
-    let namingPolicy =
-        JsonSerializerOptions(PropertyNamingPolicy = SnakeCaseNamingPolicy())
-
     let content = File.ReadAllText fileName
-    JsonSerializer.Deserialize<T>(content, namingPolicy)
+    JsonSerializer.Deserialize<T>(content, snakeCaseNamingPolicyOptions)
